d7586783e797c810c9f7aeb9a1386e250df6561f
Mariam Mahmoud <146382004+MariamMahmoud2003@users.noreply.github.com>
2025-05-14 07:45:10 -0700

    Update standard_deviation.c

--- standard_deviation.c
+++ standard_deviation.c
@@ -18,50 +18,49 @@ int main(int argc,char ** argv) {
     for(i=0;i<n;++i)
         scanf("%d",&array[i]);
     /**to calculate mean that is summation of all array members then divide it by size of array**/
-    double mean = 0;
+    double mean = 0.0;
     /**parallelize part**/
     /**
      * pragma : inform the compiler that this part will be comment and don't translate it to code
-     * omp : inform the compiler that it will use openmp 
+     * omp : inform the compiler that it will use openmp
      * parallel : this part of code will be parallel so many thread will work on it
      * for : the for loop will be parallel so we will divide iterations for threads that will work
-     * default(none) : good for localizing as only things in the parallel part of code is involved & we can not access any thing outside 
-       unless using shared or private or reduction 
-     * shared (array,n) : means that these variables are seen by all threads and can access it (work on same copy) 
+     * default(none) : good for localizing as only things in the parallel part of code is involved & we can not access any thing outside
+       unless using shared or private or reduction
+     * shared (array,n) : means that these variables are seen by all threads and can access it (work on same copy)
        so it needs synchronization to avoid race condition if any thread will update it if all read so no need for synchronous
      * private(id) : means that each thread has a copy of variable id and can not see this local variable from each other
-     * reduction(+:mean) : each process has its own private copy of sum and work on it when all finish 
+     * reduction(+:mean) : each process has its own private copy of sum and work on it when all finish
        reduction combine all these private variables in the global sum
      **/
-    #pragma omp parallel for default(none) shared(array, n) private(id) reduction(+:mean)
+#pragma omp parallel for default(none) shared(array, n) private(id) reduction(+:mean)
     for(i=0;i<n;++i) {
         id=omp_get_thread_num(); //  get thread id
-        printf("thread is working %d\n",id); // print which one is working now (not must in order)
+        printf("Thread %d is working now \n",id); // print which one is working now (not must in order)
         mean += array[i]; // sum array members
     }
     printf("\n");
     mean /= n; // get mean
-    // printf("Mean: %.2f\n", mean);
     /**to calculate variance that is summation of squared difference between array's members and mean then divide by size of array**/
-    double variance = 0; 
+    double variance = 0.0;
     /**
      * pragma : inform the compiler that this part will be comment and don't translate it to code
-     * omp : inform the compiler that it will use openmp 
+     * omp : inform the compiler that it will use openmp
      * parallel : this part of code will be parallel so many thread will work on it
      * for : the for loop will be parallel so we will divide iterations for threads that will work
-     * default(none) : good for localizing as only things in the parallel part of code is involved & we can not access any thing outside 
-       unless using shared or private or reduction 
-     * shared (array,n,mean) : means that these variables are seen by all threads and can access it (work on same copy) 
+     * default(none) : good for localizing as only things in the parallel part of code is involved & we can not access any thing outside
+       unless using shared or private or reduction
+     * shared (array,n,mean) : means that these variables are seen by all threads and can access it (work on same copy)
        so it needs synchronization to avoid race condition if any thread will update it if all read so no need for synchronous
-     * reduction(+:variance) : each process has its own private copy of sum and work on it when all finish 
+     * reduction(+:variance) : each process has its own private copy of sum and work on it when all finish
        reduction combine all these private variables in the global
      **/
-    #pragma omp parallel for default(none) shared(array, n, mean) reduction(+:variance)
+#pragma omp parallel for default(none) shared(array, n, mean) reduction(+:variance)
     for(i=0;i<n;++i)
         variance += pow(array[i]-mean,2);
     variance /= n; // calculate variance
     // standard deviation is square root of variance
-    printf("Standard Deviation: %f", sqrt(variance)); 
+    printf("Standard Deviation: %f", sqrt(variance));
     /**de-allocate memory**/
     free(array);
     return 0;
